aa77a0426aa7e3b86166fbed6979ad692d97e311
ramos <rdiego26@gmail.com>
2023-04-17 17:33:15 +0100

    fix: apply lint

--- bin/commands/getUser.js
+++ bin/commands/getUser.js
@@ -1,10 +1,11 @@
-const { fetchUserInfo } = require('../services/github');
-const { upsertUser } = require('../services/database');
+const { fetchUserInfo } = require('../services/github')
+const { upsertUser } = require('../services/database')
 
 const getUser = async (user) => {
-  const result = await fetchUserInfo(user);
-  await upsertUser(result.login, result);
-  console.info(`The data about user ${user} was storage and here is the data=${JSON.stringify(result)}`);
+  const result = await fetchUserInfo(user)
+  await upsertUser(result.login, result)
+  console.info(`The data about user ${user} was storage
+   and here is the data=${JSON.stringify(result)}`)
 }
 
 module.exports = getUser
--- bin/services/database.js
+++ bin/services/database.js
@@ -10,7 +10,11 @@ const db = pgp(cn)
 
 module.exports = {
   upsertUser: async (name, data) => {
-    await db.none('INSERT INTO users(username, data) VALUES($1, $2) ON CONFLICT(username) DO ' +
-        'UPDATE SET data = $2, updated_at = NOW()', [name, data]);
+    await db.none('INSERT INTO users(username, data) ' +
+        'VALUES($1, $2:json) ON CONFLICT(username) DO ' +
+        'UPDATE SET data = $2:json, updated_at = NOW()', [name, data])
+  },
+  fetchUsers: async () => {
+    await db.any('SELECT * FROM users')
   }
 }
