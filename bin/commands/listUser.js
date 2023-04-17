8c954d6000a052485af666695a4f21abee00e7cd
ramos <rdiego26@gmail.com>
2023-04-17 18:05:38 +0100

    refactor: implement decorator in order to reuse

--- bin/commands/listUser.js
+++ bin/commands/listUser.js
@@ -1,8 +1,17 @@
-const { fetchUsers } = require('../services/database')
+const { fetchUsers, fetchUsersByLocation } = require('../services/database')
+
+const decorate = (data) => {
+  return data.username;
+};
 
 const listUser = async () => {
-  const allData = await fetchUsers()
-  return allData.map((user) => user.username)
+  const allData = await fetchUsers();
+  return allData.map(decorate);
+}
+
+const listUserByLocation = async (location) => {
+  const allData = await fetchUsersByLocation(location);
+  return allData.map(decorate);
 }
 
 module.exports = listUser
